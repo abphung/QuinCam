ffecd66e085983b45fa7eb37699a1f334ce31c83
Andrew Phung <phung8128@gmail.com>
2019-12-22 11:41:59 -0800

    got http server to work, but camera is now broken

--- Arduino/QuinCam/QuinCam.ino
+++ Arduino/QuinCam/QuinCam.ino
@@ -4,6 +4,9 @@
 #include <list>
 #include <map> 
 
+#include "esp_http_server.h"
+#include <WiFi.h>
+
 // Select camera model
 //#define CAMERA_MODEL_WROVER_KIT
 //#define CAMERA_MODEL_ESP_EYE
@@ -17,6 +20,14 @@
 /** the current address in the EEPROM (i.e. which byte we're going to write to next) **/
 int address = 0;
 BluetoothSerial SerialBT;
+
+const char* ssid = "Andrew's Wi-Fi Network";
+const char* password = "itsonthefridge";
+typedef struct {
+        httpd_req_t *req;
+        size_t len;
+} jpg_chunking_t;
+httpd_handle_t camera_httpd = NULL;
 
 //////////////////
 //Setup functions
@@ -72,7 +83,7 @@ void InitializeCamera() {
   //init with high specs to pre-allocate larger buffers
   if (psramFound()) {
     Serial.println("psram found");
-    config.frame_size = FRAMESIZE_XGA;
+    config.frame_size = FRAMESIZE_QXGA;
     config.jpeg_quality = 10;
     config.fb_count = 2;
   } else {
@@ -194,6 +205,71 @@ void take_picture() {
   esp_camera_fb_return(fb);
 }
 
+static size_t jpg_encode_stream(void * arg, size_t index, const void* data, size_t len){
+    jpg_chunking_t *j = (jpg_chunking_t *)arg;
+    if(!index){
+        j->len = 0;
+    }
+    if(httpd_resp_send_chunk(j->req, (const char *)data, len) != ESP_OK){
+        return 0;
+    }
+    j->len += len;
+    return len;
+}
+
+static esp_err_t capture_handler(httpd_req_t *req){
+    camera_fb_t * fb = NULL;
+    esp_err_t res = ESP_OK;
+    int64_t fr_start = esp_timer_get_time();
+
+    fb = esp_camera_fb_get();
+    if (!fb) {
+        Serial.println("Camera capture failed");
+        httpd_resp_send_500(req);
+        return ESP_FAIL;
+    }
+
+    httpd_resp_set_type(req, "image/jpeg");
+    httpd_resp_set_hdr(req, "Content-Disposition", "inline; filename=capture.jpg");
+
+    size_t out_len, out_width, out_height;
+    uint8_t * out_buf;
+    bool s;
+    bool detected = false;
+    int face_id = 0;
+
+    size_t fb_len = 0;
+    if(fb->format == PIXFORMAT_JPEG){
+        fb_len = fb->len;
+        res = httpd_resp_send(req, (const char *)fb->buf, fb->len);
+    } else {
+        jpg_chunking_t jchunk = {req, 0};
+        res = frame2jpg_cb(fb, 80, jpg_encode_stream, &jchunk)?ESP_OK:ESP_FAIL;
+        httpd_resp_send_chunk(req, NULL, 0);
+        fb_len = jchunk.len;
+    }
+    esp_camera_fb_return(fb);
+    int64_t fr_end = esp_timer_get_time();
+    Serial.printf("JPG: %uB %ums\n", (uint32_t)(fb_len), (uint32_t)((fr_end - fr_start)/1000));
+    return res;
+}
+
+void startCameraServer(){
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+    httpd_uri_t capture_uri = {
+        .uri       = "/capture",
+        .method    = HTTP_GET,
+        .handler   = capture_handler,
+        .user_ctx  = NULL
+    };
+
+    Serial.printf("Starting web server on port: '%d'\n", config.server_port);
+    if (httpd_start(&camera_httpd, &config) == ESP_OK) {
+        httpd_register_uri_handler(camera_httpd, &capture_uri);
+    }
+}
+
 void TestBluetooth(char** argv) {
   uint8_t* rand_pointer;
   Serial.printf("Writing %d bytes to bluetooth", *argv[0]);
@@ -212,7 +288,23 @@ void UpdateConfigs(char** argv) {
 void setup() {
   Serial.begin(115200);
   Serial.println();
-  InitializeBluetooth();
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+  
+  //InitializeBluetooth();
   InitializeCamera();
 }
 
